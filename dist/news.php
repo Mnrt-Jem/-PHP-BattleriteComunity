33525c2c2bfa1721f66da3a7538e809afa2a17a0
Jean-Emile MINART <j-em@outlook.fr>
2017-06-27 13:19:18 +0200

    Update news.php
    système de pagination

--- dist/news.php
+++ dist/news.php
@@ -1,6 +1,19 @@
 <?php include "assets/include/header.php" ?>
 <?php include "assets/include/cookieconnect.php" ?>
-
+<?php 
+$articleparpage = 3;
+$articleTotal = $con->query('SELECT id_news from news');
+$patchTotal = $articleTotal->rowCount();
+$pagesTotales = ceil($patchTotal/$articleparpage);
+if (isset($_GET['page']) AND !empty($_GET['page']) AND $_GET['page'] > 0 AND $_GET['page'] <= $pagesTotales) {
+    $_GET['page'] = intval($_GET['page']);
+    $pagecourante= $_GET['page'];
+} else
+{
+    $pagecourante = 1;
+}
+$depart = ($pagecourante-1)*$articleparpage;
+?>
 
 
     <div class="nk-main">
@@ -42,7 +55,7 @@
             <div class="nk-blog-fullwidth">
 
             <?php
-                $news=$con->prepare( 'SELECT title_news,news,id_news FROM news order by id_news desc limit 0,6');
+                $news=$con->prepare( 'SELECT title_news,news,id_news FROM news order by id_news desc limit '.$depart.','.$articleparpage);
                 $news ->execute();
                 while ($new=$news->fetch()) { ?>
                 <!-- START: Post -->
@@ -68,18 +81,20 @@
                 <?php } ?>
                 
                 <!-- START: Pagination -->
-                <!--<div class="nk-pagination nk-pagination-center">
-                    <a href="#" class="nk-pagination-prev">
-                        <span class="ion-ios-arrow-back"></span>
-                    </a>
-                    <nav>
-                        <a class="nk-pagination-current" href="#">1</a>
-                    </nav>
-                    <a href="#" class="nk-pagination-next">
-                        <span class="ion-ios-arrow-forward"></span>
-                    </a>
-                </div>-->
-                <!-- END: Pagination -->
+                        <div class="nk-pagination nk-pagination-center">
+
+                            <nav>
+                            <?php 
+                            for ($i=1; $i<=$pagesTotales ; $i++) {
+                                if($i == $pagecourante) { echo '<span class="nk-pagination-current">'.$pagecourante.'</span>'; }
+                                else { ?>
+                                <a href="news.php?page=<?=$i ?>"><?=$i ?></a>
+                                <?php } ?>
+                            <?php } ?>
+                            </nav>
+                        </div>
+
+                        <!-- END: Pagination -->
             </div>
             <!-- END: Posts FullWidth -->
         </div>
